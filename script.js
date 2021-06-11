55ca943a3ccf76c829cb271acc25597d66bad5db
sarvesh-kr-sri <sksarvesh38@gmail.com>
2021-06-11 10:12:41 +0530

    FashionHub completed

---
+++ script.js
@@ -0,0 +1,10 @@
+function change()
+{
+    document.getElementById("chng").style.backgroundImage="url('../IMG/2.jpg')";
+    document.getElementById("chng").style.transition=".3s";
+}
+function rtn()
+{
+    document.getElementById("chng").style.backgroundImage="url('../IMG/3.jpg')";
+    document.getElementById("chng").style.transition=".3s";
+}
